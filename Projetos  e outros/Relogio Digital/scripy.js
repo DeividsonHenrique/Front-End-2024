f20e776dd7c948f7364f71c2c95c1cbc68458c4c
Deividson Henrique <109252541+DeividsonHenrique@users.noreply.github.com>
2023-05-29 06:18:19 -0300

    Day 28

---
+++ Projetos  e outros/Relogio Digital/scripy.js
@@ -0,0 +1,20 @@
+const horas = document.getElementById('horas');
+const minutos = document.getElementById('minutos');
+const segundos = document.getElementById('segundos');
+
+const relogio = setInterval(function time() {
+    let datetoday = new Date();
+    let hr = datetoday.getHours();
+    let min = datetoday.getMinutes();
+    let s = datetoday.getSeconds();
+
+    if (hr < 10) hr = '0' + hr;
+
+    if (min < 10) min = '0' + min;
+
+    if (s < 10) s = '0' + s;
+
+    horas.textContent = hr;
+    minutos.textContent = min;
+    segundos.textContent = s;
+})
