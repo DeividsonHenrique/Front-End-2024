9c0583ddeba311e5644eaaecd83a75b78afc39e6
Deividson Henrique <109252541+DeividsonHenrique@users.noreply.github.com>
2024-03-03 19:36:33 -0300

    day 03.03.24

---
+++ Udemy/JavaScript/aula2/script.js
@@ -0,0 +1,15 @@
+//string
+var texto = 'Curso de JavaScript'
+
+//number
+var numeroInteiro = -7
+var numeroFracionado = 11210.75
+
+//boolean
+var teste = true //true -> Verdadeiro / False -> falso
+
+//alert(numeroFracionado) //abrir um dialog
+
+//document.write(texto)
+
+console.log(numeroInteiro)
