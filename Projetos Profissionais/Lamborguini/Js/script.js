b4e03cca35a1cc8b025cf7cf696d192924545457
Deividson Henry <deividson.henrique2016@hotmail.com>
2024-03-23 21:12:46 -0300

    day 23.03.24

--- Projetos Profissionais/Lamborguini/Js/script.js
+++ Projetos Profissionais/Lamborguini/Js/script.js
@@ -13,3 +13,19 @@ function nextImage(){
 
     document.getElementById("radio"+count).checked = true
 }
+
+
+
+window.addEventListener('scroll', function() {
+    var element = document.getElementById('fixedElement');
+    var scrollPosition = window.scrollY;
+  
+    // Define o limite de rolagem para fazer o elemento desaparecer
+    var scrollLimit = 800;
+  
+    if (scrollPosition > scrollLimit) {
+      element.classList.add('hidden'); // Adiciona a classe 'hidden' para tornar o elemento invisível
+    } else {
+      element.classList.remove('hidden'); // Remove a classe 'hidden' para tornar o elemento visível
+    }
+  });
