2a5261a92bad079fc64055f64024d52460b0b3f3
Deividson Henry <deividson.henrique2016@hotmail.com>
2024-03-27 22:55:52 -0300

    day 27.03.24

---
+++ Curso em Vídeo/Curso em video JAVASCRIPT/aula5/ambiente.js
@@ -0,0 +1,32 @@
+var love = 1
+while (love <= 5){
+    console.log(`Te Amo ${love} Vezes!`)
+    love++
+}
+
+
+
+/*var c = 1
+do{
+    console.log(`Passo ${c}`)
+    c++
+}while (c <= 6)*/
+
+
+/*var c = 1
+while (c <= 6){
+    console.log(`Passo ${c}`)
+    c++
+}
+ */
+
+
+
+/*
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')*/
